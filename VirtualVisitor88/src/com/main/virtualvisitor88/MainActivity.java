81061fde5390c538419043166485813a7a51f3e2
Shun Hosaka <ee27025@meiji.ac.jp>
2014-02-09 15:25:43 +0900

    15時の改修アップ

--- VirtualVisitor88/src/com/main/virtualvisitor88/MainActivity.java
+++ VirtualVisitor88/src/com/main/virtualvisitor88/MainActivity.java
@@ -1,19 +1,91 @@
 package com.main.virtualvisitor88;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class MainActivity extends Activity{
 
 	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private CharSequence mTitle;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mTitle = getTitle();
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		
+		// enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+		
+		mDrawerToggle = new ActionBarDrawerToggle(
+				this,
+				mDrawerLayout,
+                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close){
+                	  
+                /** Called when a drawer has settled in a completely closed state. */  
+                public void onDrawerClosed(View view) {  
+                        setTitle(mTitle);  
+                    }  
+          
+                    /** Called when a drawer has settled in a completely open state. */  
+                    public void onDrawerOpened(View drawerView) {  
+                        setTitle(mTitle);  
+                    }  
+			};
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 	}
+	
+	@Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = false;
+        return super.onPrepareOptionsMenu(menu);
+    }
+	
+	@Override
+	public void setTitle(CharSequence title) {
+	    mTitle = title;
+	    getActionBar().setTitle(mTitle);
+	}
+	
+	@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+    @Override  
+    public boolean onOptionsItemSelected(MenuItem item) {  
+        // Pass the event to ActionBarDrawerToggle, if it returns  
+        // true, then it has handled the app icon touch event  
+        if (mDrawerToggle.onOptionsItemSelected(item)) {  
+          return true;  
+        }  
+        // Handle your other action bar items...  
+  
+        return super.onOptionsItemSelected(item);  
+    }
 
 }
