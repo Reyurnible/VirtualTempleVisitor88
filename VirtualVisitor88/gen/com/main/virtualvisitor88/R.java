90c0881efd24e23fd1503bec267c74c7baf66b9c
akihiro oguro <oguro1021@gmail.com>
2014-02-10 02:23:42 +0900

    resolve conflict

--- VirtualVisitor88/gen/com/main/virtualvisitor88/R.java
+++ VirtualVisitor88/gen/com/main/virtualvisitor88/R.java
@@ -27,25 +27,38 @@ public final class R {
     public static final class drawable {
         public static final int ic_drawer=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int temple=0x7f020002;
+        public static final int map=0x7f020002;
+        public static final int parts_map=0x7f020003;
+        public static final int shikoku=0x7f020004;
+        public static final int temple=0x7f020005;
+        public static final int tera=0x7f020006;
+        public static final int tera2=0x7f020007;
     }
     public static final class id {
+        public static final int LinearLayout1=0x7f08000c;
         public static final int content_frame=0x7f080008;
         public static final int drawer_layout=0x7f080007;
+        public static final int imageMap=0x7f080009;
+        public static final int imageView1=0x7f08000b;
         public static final int infoDay=0x7f080000;
         public static final int infoDistance=0x7f080002;
         public static final int infoNextTemple=0x7f080005;
         public static final int infoNextTempleDistance=0x7f080006;
         public static final int infoSteps=0x7f080001;
         public static final int infoTemple=0x7f080003;
-        public static final int left_drawer=0x7f080009;
+        public static final int left_drawer=0x7f08000a;
         public static final int templeImage=0x7f080004;
-        public static final int value_id=0x7f08000a;
+        public static final int textView1=0x7f08000d;
+        public static final int textView2=0x7f08000e;
+        public static final int textView3=0x7f08000f;
+        public static final int value_id=0x7f080010;
     }
     public static final class layout {
         public static final int activity_drawer=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int activity_setting=0x7f030002;
+        public static final int activity_map=0x7f030002;
+        public static final int activity_setting=0x7f030003;
+        public static final int info_window_activity=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
