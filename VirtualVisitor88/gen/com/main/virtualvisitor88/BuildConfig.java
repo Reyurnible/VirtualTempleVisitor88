e90208bac380e3c771e7383893084bcabad77e16
Shun Hosaka <ee27025@meiji.ac.jp>
2014-02-09 12:42:52 +0900

    first commit

---
+++ VirtualVisitor88/gen/com/main/virtualvisitor88/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.main.virtualvisitor88;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ VirtualVisitor88/src/com/main/virtualvisitor88/MainActivity.java
@@ -0,0 +1,19 @@
+package com.main.virtualvisitor88;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+
+public class MainActivity extends Activity{
+
+	private DrawerLayout mDrawerLayout;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+	}
+
+}
