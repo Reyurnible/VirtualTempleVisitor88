7ecb8c819607eef18def7a20246b48497d012928
akihiro oguro <oguro1021@gmail.com>
2014-02-09 14:32:57 +0900

    complete service class and accel class

--- VirtualVisitor88/src/com/main/virtualvisitor88/MainActivity.java
+++ VirtualVisitor88/src/com/main/virtualvisitor88/MainActivity.java
@@ -3,20 +3,26 @@ package com.main.virtualvisitor88;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.hardware.SensorManager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 
 public class MainActivity extends Activity{
 
 	private DrawerLayout mDrawerLayout;
-	
+	SensorManager	sm;
+	WalkCount		wc;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
 		startService( new Intent( MainActivity.this, WalkService.class ) );
+		stopService();
 	}
-
+	
+	void stopService(){
+		stopService( new Intent( MainActivity.this, WalkService.class ) );
+	}
 }
--- VirtualVisitor88/src/com/main/virtualvisitor88/WalkService.java
+++ VirtualVisitor88/src/com/main/virtualvisitor88/WalkService.java
@@ -1,25 +1,38 @@
 package com.main.virtualvisitor88;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.SensorManager;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 public class WalkService extends Service {
-
+	
+	Handler mHandler = new Handler();
+	SensorManager	sm;
+	WalkCount		wc;
+	
 	@Override
 	public void onCreate() {
-		Log.i("TestService", "onCreate");
+		Log.i("WalkService", "onCreate");
 	}
+	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Log.i("TestService", "onStartCommand");
+		sm = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+		wc = new WalkCount(sm);
 		return START_STICKY;
 	}
 
 	@Override
 	public void onDestroy() {
 		Log.i("TestService", "onDestroy");
+		wc.stop();
+		Toast.makeText(this, "MyServiceÅ@onDestroy", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
