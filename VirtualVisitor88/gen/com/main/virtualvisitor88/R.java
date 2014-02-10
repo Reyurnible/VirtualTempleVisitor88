537536632de81ae2388aaeffd381b24ccd8fb02e
akihiro oguro <oguro1021@gmail.com>
2014-02-10 10:20:19 +0900

    add setting layout

--- VirtualVisitor88/gen/com/main/virtualvisitor88/R.java
+++ VirtualVisitor88/gen/com/main/virtualvisitor88/R.java
@@ -9,8 +9,7 @@ package com.main.virtualvisitor88;
 
 public final class R {
     public static final class array {
-        public static final int entries_list_preference=0x7f090000;
-        public static final int entryvalues_list_preference=0x7f090001;
+        public static final int sex=0x7f040000;
     }
     public static final class attr {
     }
@@ -21,8 +20,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_drawer=0x7f020000;
@@ -35,23 +34,24 @@ public final class R {
         public static final int tera2=0x7f020007;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f08000c;
-        public static final int content_frame=0x7f080008;
-        public static final int drawer_layout=0x7f080007;
-        public static final int imageMap=0x7f080009;
-        public static final int imageView1=0x7f08000b;
-        public static final int infoDay=0x7f080000;
-        public static final int infoDistance=0x7f080002;
-        public static final int infoNextTemple=0x7f080005;
-        public static final int infoNextTempleDistance=0x7f080006;
-        public static final int infoSteps=0x7f080001;
-        public static final int infoTemple=0x7f080003;
-        public static final int left_drawer=0x7f08000a;
-        public static final int templeImage=0x7f080004;
-        public static final int textView1=0x7f08000d;
-        public static final int textView2=0x7f08000e;
-        public static final int textView3=0x7f08000f;
-        public static final int value_id=0x7f080010;
+        public static final int LinearLayout1=0x7f09000c;
+        public static final int content_frame=0x7f090008;
+        public static final int drawer_layout=0x7f090007;
+        public static final int imageMap=0x7f090009;
+        public static final int imageView1=0x7f09000b;
+        public static final int infoDay=0x7f090000;
+        public static final int infoDistance=0x7f090002;
+        public static final int infoNextTemple=0x7f090005;
+        public static final int infoNextTempleDistance=0x7f090006;
+        public static final int infoSteps=0x7f090001;
+        public static final int infoTemple=0x7f090003;
+        public static final int left_drawer=0x7f09000a;
+        public static final int resetbutton=0x7f090010;
+        public static final int templeImage=0x7f090004;
+        public static final int textView1=0x7f09000d;
+        public static final int textView2=0x7f09000e;
+        public static final int textView3=0x7f09000f;
+        public static final int value_id=0x7f090011;
     }
     public static final class layout {
         public static final int activity_drawer=0x7f030000;
@@ -59,16 +59,17 @@ public final class R {
         public static final int activity_map=0x7f030002;
         public static final int activity_setting=0x7f030003;
         public static final int info_window_activity=0x7f030004;
+        public static final int resetbutton=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int drawer_close=0x7f050004;
-        public static final int drawer_open=0x7f050003;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int drawer_close=0x7f060004;
+        public static final int drawer_open=0x7f060003;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -92,10 +93,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- VirtualVisitor88/src/com/main/virtualvisitor88/SettingActivity.java
+++ VirtualVisitor88/src/com/main/virtualvisitor88/SettingActivity.java
@@ -2,11 +2,14 @@ package com.main.virtualvisitor88;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.widget.Button;
 
-public class SettingActivity extends Activity{
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_setting);
-	}
+public class SettingActivity extends PreferenceActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.layout.activity_setting);
+
+    }
 }
