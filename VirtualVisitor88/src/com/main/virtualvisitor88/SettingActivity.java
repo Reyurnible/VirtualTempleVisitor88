4968d938c72ccd2ab693fd578282794bb12835c3
akihiro oguro <oguro1021@gmail.com>
2014-02-11 11:19:48 +0900

    save state

--- VirtualVisitor88/src/com/main/virtualvisitor88/SettingActivity.java
+++ VirtualVisitor88/src/com/main/virtualvisitor88/SettingActivity.java
@@ -44,6 +44,7 @@ public class SettingActivity extends PreferenceActivity implements OnSharedPrefe
 				 
 				    public void onClick(DialogInterface dialog, int which) {
 				    	MainActivity.walkCount = 0;
+
 				    }
 				});
 				// NGボタンの設定
